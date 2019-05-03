93a25048d23bc7b96b9236092ac9b7822ad90b7f
Kristoffel Pirard <sahsioux06@softathome.com>
2019-05-03 13:06:14 +0200

    Break infinite loop...

--- gitlab-ci-job-timing/visualize_timings.js
+++ gitlab-ci-job-timing/visualize_timings.js
@@ -3,12 +3,18 @@ Just draw a border round the document.body.
 */
 document.body.style.border = "1px solid red";
 
-$("body").on("DOMSubtreeModified", ".js-job-content", function(){
-    $("[data-action='end']").removeClass("hidden").addClass("alert alert-info").html(function(index, val){
-        var section =$(this).data('section');
-        var start = $("[data-action='start'][data-section='"+section+"']");
-        var startT = start.data('timestamp');
-        var endT = $(this).data('timestamp');
-        return section + ", time used: " + (endT - startT) + "s";
+$("body").ready(function(){
+    var appending = {busy: false};
+    $("#content-body").on("DOMSubtreeModified", ".js-job-content", appending, function(){
+        if (appending.busy) return;
+        appending.busy = true;
+        $("[data-action='end']").removeClass("hidden").addClass("alert alert-info").html(function(index, val){
+            var section =$(this).data('section');
+            var start = $("[data-action='start'][data-section='"+section+"']");
+            var startT = start.data('timestamp');
+            var endT = $(this).data('timestamp');
+            return section + ", time used: " + (endT - startT) + "s";
+        });
+        appending.busy = false;
     });
 });
