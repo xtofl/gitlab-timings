cff162d5ecb4da542e7a93c669b2073ddcee53bd
Kristoffel Pirard <sahsioux06@softathome.com>
2019-05-03 11:23:53 +0200

    deal with async loading of job content
    The gitlab job content is loaded asynchronously.  Therefor
    we need to attach ourselves to the appropriate change and
    reapply the timing element insertion

--- gitlab-ci-job-timing/visualize_timings.js
+++ gitlab-ci-job-timing/visualize_timings.js
@@ -1,12 +1,14 @@
 /*
 Just draw a border round the document.body.
 */
-document.body.style.border = "5px solid red";
+document.body.style.border = "1px solid red";
 
-$("[data-action='end']").removeClass("hidden").addClass("alert alert-info").html(function(index, val){
-    var section =$(this).data('section');
-    var start = $("[data-action='start'][data-section='"+section+"']");
-    var startT = start.data('timestamp');
-    var endT = $(this).data('timestamp');
-    return section + ", time used: " + (endT - startT) + "s";
+$("body").on("DOMSubtreeModified", ".js-job-content", function(){
+    $("[data-action='end']").removeClass("hidden").addClass("alert alert-info").html(function(index, val){
+        var section =$(this).data('section');
+        var start = $("[data-action='start'][data-section='"+section+"']");
+        var startT = start.data('timestamp');
+        var endT = $(this).data('timestamp');
+        return section + ", time used: " + (endT - startT) + "s";
+    });
 });
